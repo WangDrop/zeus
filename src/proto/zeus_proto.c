3854f8c1e3e6c5cda45cba0137f10a806ab44488
y5a9f2y <ydzhao1992@gmail.com>
2015-06-05 23:52:48 +0800

    update proto read part

--- src/proto/zeus_proto.c
+++ src/proto/zeus_proto.c
@@ -18,28 +18,27 @@ zeus_status_t zeus_proto_solve_read_buf(zeus_process_t *p,zeus_event_t *ev){
     zeus_uchar_t opcode;
     zeus_uint_t len;
     zeus_connection_t *conn;
-    zeus_size_t leftlen;
 
-    if(ev->buflen > 0){
+    zeus_proto_helper_get_opcode_and_pktlen(ev,&opcode,&len);
 
-        zeus_proto_helper_get_opcode_and_pktlen(ev,&opcode,&len);
-
-        switch(opcode){
-            case ZEUS_PROTO_DATA_INS:
-
-                break;
-            default:
-                conn = ev->connection;
-                zeus_write_log(p->log,ZEUS_LOG_ERROR,"%s:%hu send a wrong opcode : %d",\
-                                                     conn->addr_string->data,          \
-                                                     ntohs(conn->peer->sin_port),      \
-                                                     opcode);
-                return ZEUS_ERROR;
-        }
-
-    }else{
-        return ZEUS_OK;
+    switch(opcode){
+        case ZEUS_PROTO_DATA_INS:
+            if(ev->buflen - ZEUS_PROTO_OPCODE_SIZE - ZEUS_PROTO_DATA_LEN_SIZE > len){
+                // TODO
+                // TRANS THE DATA TO THE ADMIN CLIENT
+                // RECYCLE BUFFER
+            }
+            break;
+        default:
+            conn = ev->connection;
+            zeus_write_log(p->log,ZEUS_LOG_ERROR,"%s:%hu send a wrong opcode : %d",\
+                                                 conn->addr_string->data,          \
+                                                 ntohs(conn->peer->sin_port),      \
+                                                 opcode);
+            return ZEUS_ERROR;
     }
+
+    return ZEUS_OK;
 
 }
 
